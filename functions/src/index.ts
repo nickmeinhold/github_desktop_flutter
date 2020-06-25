fc7c10c055c3b7fedb26e1510ee2b56950daf027
Nick Meinhold <nick.meinhold@gmail.com>
2020-06-25 13:37:32 +1000

    Access to fetch from origin has been blocked by CORS policy (#18)
    Fixes #17
    
    - updated cloud function to use express app with cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,50 @@
-import * as functions from 'firebase-functions';
-const cors = require('cors')({origin: true,});
-const rp = require('request-promise');
+// --- Relevant resources ---
+//
+// firebase/functions-samples > authorized-https-endpoint
+// https://github.com/firebase/functions-samples/blob/master/authorized-https-endpoint/functions/index.js
+// 
+// cors 
+// https://github.com/expressjs/cors
+// 
+// Using cors with typescript 
+// https://brianflove.com/2017-03-22/express-cors-typescript/
+// 
+// Using existing express apps in cloud functions 
+// https://firebase.google.com/docs/functions/http-events#using_existing_express_apps
 
-export const getToken = functions.https.onRequest((request, response) => {
-    // Enable CORS using the `cors` express middleware.
-    return cors(request, response, async () => {
-        try {
-            const auth_token = await exchangeCodeForAuthToken(request.get('code'));
-            return response.send(auth_token);
-          } catch(error) {
-            console.error(error);
-            return response.status(500).send('Something went wrong while exchanging the code.');
-          }
-    });
-});
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as cors from 'cors';
+import * as rp from 'request-promise';
+
+const app = express();
+
+export const getToken = functions.https.onRequest(app);
+
+// get the code from the request, call retrieveAuthToken and return the response
+const exchangeCodeForAuthToken = async (req: any, resp: any) => {
+  try {
+    const auth_token = await retrieveAuthToken(req.get('code'));
+    return resp.send(auth_token);
+  } catch(error) {
+    console.error(error);
+    return resp.status(500).send('Something went wrong while exchanging the code.');
+  }
+};
 
 // Exchange the code from github (plus a secret) for an auth token 
-function exchangeCodeForAuthToken(code: string = '') {
-    return rp({
-      method: 'POST',
-      uri: 'https://github.com/login/oauth/access_token',
-      body: {
-        code: code,
-        client_id: functions.config().github.client_id,
-        client_secret: functions.config().github.client_secret,
-      },
-      json: true,
-    });
+function retrieveAuthToken(code: string = '') {
+  return rp({
+    method: 'POST',
+    uri: 'https://github.com/login/oauth/access_token',
+    body: {
+      code: code,
+      client_id: functions.config().github.client_id,
+      client_secret: functions.config().github.client_secret,
+    },
+    json: true,
+  });
 }
+
+app.use(cors());
+app.use(exchangeCodeForAuthToken);
